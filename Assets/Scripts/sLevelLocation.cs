699b90ecc0e7678c419396e5dc4bb69ac2fed2bc
Arshnoor <arshnoor88@gmail.com>
2023-10-15 17:51:56 -0400

    Mascot and Levels Completely Prepared

--- Assets/Scripts/sLevelLocation.cs
+++ Assets/Scripts/sLevelLocation.cs
@@ -1,17 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Splines;
 
 public class sLevelLocation : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro levelCountLabel;
-    [SerializeField] private BoxCollider boxDetector;
-
-    private int levelCount;
+    [Header("Set Flag Number")]
+    [SerializeField] private int flagLevel;
+    
+    [Header("First and Last Selection")]
+    [SerializeField] private bool isFirst;
+    [SerializeField] private bool isLast;
+    
+    [Header("DO NOT CHANGE")]
+    [SerializeField] private Material[] mats_FL;
+    [SerializeField] private TextMeshPro levelCountLabelF;
+    [SerializeField] private TextMeshPro levelCountLabelB;
+    [SerializeField] private MeshRenderer flagRenderer;
+    [SerializeField] private SplineContainer associatedSpline;
+    
     private sSceneController sceneController;
     private sMascotController mascot;
 
+    //Unity Start Event
     private void Start()
     {
         //get scene controller reference
@@ -19,30 +29,63 @@ public class sLevelLocation : MonoBehaviour
 
         //get Mascot Reference
         mascot = FindFirstObjectByType<sMascotController>();
-
-        //setting the level count from the scene controller
-        levelCount = sceneController.GetCurrentLevel();
+        
+        //Generates Label At Level Start for all instances
+        FlagInitialisation();
+    }
+    
+    //Return the start Position of the associated Spline
+    public Vector3 ReturnSplineStart()
+    {
+        return associatedSpline.EvaluatePosition(0);
     }
 
-    //Sets the level number on the 3D viewports
-    public void SetLevelCount(int count)
+    public Vector3 ReturnSplineEnd()
     {
-        levelCountLabel.text = count.ToString();
-        levelCount = count;
+        if (!isLast)
+            return associatedSpline.EvaluatePosition(1);
+        else
+            return transform.position;
     }
 
-
-    private void OnCollisionEnter(Collision collision)
+    //Generates Label and Start for all instances
+    private void FlagInitialisation()
     {
-        Debug.Log("Collision Detected");
-        if(collision.gameObject.CompareTag("Mascot"))
+        if (isFirst)
         {
-            if(sceneController.GetCurrentLevel()>1)
-            {
-                sceneController.NewLevelReached();
-                Debug.Log("New Level");
-            }
-            Debug.Log("Level 1");
+            flagRenderer.sharedMaterials[1] = mats_FL[0];
+            levelCountLabelF.text = "1";
+            levelCountLabelB.text = "1";
         }
+        else if(isLast)
+        {
+            flagRenderer.sharedMaterials[1] = mats_FL[1];
+            Debug.Log("Is Last Executed");
+            levelCountLabelF.text = flagLevel.ToString();
+            levelCountLabelB.text = flagLevel.ToString();
+        }
+        else
+        {
+            levelCountLabelF.text = flagLevel.ToString();
+            levelCountLabelB.text = flagLevel.ToString();
+        }
+    }
+    
+    // ----- MAY NEED TO BE DEPRECATED ---- Event Executed when the Mascot enters the Trigger of the next Flag
+    void OnTriggerEnter(Collider other)
+    {
+        //Do something on Entering the new level
+    }
+    
+    //Returns the Associated Spline
+    public SplineContainer GetAssociatedSpline()
+    {
+        return associatedSpline;
+    }
+    
+    //Returns the value of the public boolean to check if this is the last flag
+    public bool isFinal()
+    {
+        return isLast;
     }
 }
--- Assets/Scripts/sMascotController.cs
+++ Assets/Scripts/sMascotController.cs
@@ -5,37 +5,21 @@ public class sMascotController : MonoBehaviour
 {
     [SerializeField] private SplineAnimate mascotSplineAnimate;
     
-    //Variable for Character Speed on Spline
-    public float movementSpeed = 20;
-
-    private SplineContainer currentSpline;
-    private SplineContainer nextSpline;
-
-    private void Awake()
-    {
-        //mascotSplineAnimate.Pause();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentSpline = mascotSplineAnimate.Container;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     //Function will be called by the SceneController when the player will have to move
     public void MoveMascot()
     {
+        mascotSplineAnimate.Restart(false);
         mascotSplineAnimate.Play();
     }
 
-    public void ChangeMascotSpline()
+    public void SetMascotSpline(SplineContainer splineContainer)
     {
-        mascotSplineAnimate.Container = nextSpline;
+        mascotSplineAnimate.Container = splineContainer;
+    }
+
+    public void JumpToNewChapter(Vector3 jumpStart, Vector3 jumpEnd, SplineContainer targetSpline)
+    {
+        mascotSplineAnimate.Container = targetSpline;
+        mascotSplineAnimate.Restart(false);
     }
 }
